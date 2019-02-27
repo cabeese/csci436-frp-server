43792466f82d24e79bb59e2547d5968b9e49e268
Noah <strongn@wwu.edu>
2019-02-26 18:10:28 -0800

    Initial 'donation' and 'donation_item' models

---
+++ api/models/Donation.js
@@ -0,0 +1,56 @@
+/**
+ * Donation.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    claimed: {
+      type: "boolean",
+      required: true,
+      description: "True when all associated items have been claimed. Must be set by Controller."
+    },
+
+    location: {
+      type: "string",
+      description: "Where the donation is. (Address? GPS Coords?)",
+    },
+
+    contactPhone: {
+      type: "number",
+      description: "A phone number someone can call with questions about the donation.",
+    },
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+    // For information on linking documents (using "associations"), see
+    // https://sailsjs.com/documentation/concepts/models-and-orm/associations/one-to-many
+
+    items: {
+      collection: "donation_item",
+      via: "parent"
+    },
+
+    owner: {
+      model: "user",
+    },
+
+  },
+
+};
+
---
+++ api/models/Donation_Item.js
@@ -0,0 +1,58 @@
+/**
+ * Donation_item.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    foodName: {
+      type: "string",
+      required: true,
+      description: "The type of food in this line item (e.g. 'blueberries' or 'fruits')"
+    },
+
+    initialQty: {
+      type: "number",
+      required: true,
+      description: "The amount of this item available initially"
+    },
+
+    remainingQty: {
+      type: "number",
+      required: true,
+      description: "The amount of this item still available (based on how much has been claimed)"
+    },
+
+    qtyUnits: {
+      type: "string",
+      isIn: ["crates", "pallets", "pounds"],
+      required: true,
+      description: "The units to use when referring to the quantity of the item"
+    },
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+    parent: {
+      model: "donation"
+    }
+
+  },
+
+};
+
