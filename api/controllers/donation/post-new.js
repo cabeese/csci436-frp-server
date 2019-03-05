ad2a94d81ad5b280633a908716652100d0f780a9
Noah <strongn@wwu.edu>
2019-03-04 20:36:36 -0800

    Add errors and other validation to /donations API

--- api/controllers/donation/post-new.js
+++ api/controllers/donation/post-new.js
@@ -17,27 +17,53 @@ module.exports = {
 
 
   exits: {
-
+    noItems: {
+      statusCode: 400,
+    },
+    invalidItems: {
+      statusCode: 400,
+    }
   },
 
 
-  fn: async function (inputs) {
-
+  fn: async function (inputs, exits) {
     let {donation} = inputs;
     let {items} = donation;
     donation.items = [];
 
+    // Complain if the 'items' array is empty
+    if(items.length === 0){
+      return exits.noItems({
+        error: true,
+        message: "The 'items' field was empty - please include items to donate!",
+      });
+    }
+
+    // First, save the parent Donation object
     let {id} = await Donation.create(donation).fetch();
 
+    // Now give each donation_item the appropriate parent id
     items.forEach(function(item){
       item.parent = id;
     });
 
-    let savedItems = await Donation_Item.createEach(items).fetch();
+    // Save all the donation items
+    let savedItems;
+    try {
+      savedItems = await Donation_Item.createEach(items).fetch();
+    } catch(e){
+      // Shoot - one of the items was invalid. Destroy this record and complain to user
+      let {details} = e;
+      await Donation.destroy({id: id}).fetch();
+      return exits.invalidItems({
+        error: true,
+        message: details,
+      });
+    }
 
-    // TODO:
-    // * set 'isClaimed' property on parent donation
-    // * handle errors if some of these are bad (delete all saved)
+    // Update the 'claimed' status of the new donation
+    let claimed = sails.helpers.donationIsClaimed(savedItems);
+    await Donation.updateOne({id: id}).set({claimed: claimed});
 
     // All done.
     return;
