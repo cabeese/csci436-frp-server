78cc30c64382881ba7a703d7e3ad48dae70dcad1
Noah <strongn@wwu.edu>
2019-05-13 19:44:48 -0700

    Initial donation-admin page

---
+++ api/controllers/dashboard/view-donation-admin.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+
+  friendlyName: 'View donation admin',
+
+
+  description: 'Display "Donation admin" page.',
+
+
+  exits: {
+
+    success: {
+      viewTemplatePath: 'pages/dashboard/donation-admin'
+    }
+
+  },
+
+ fn: async function () {
+    donations = await Donation.find({}).populate("items");
+
+    // Respond with view.
+    return {
+      donations
+    };
+
+  }
+
+
+};
